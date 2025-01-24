c799f0b201ad419c99029d798e15f6da992c4d30
Juha <juhaluoma5@gmail.com>
2025-01-24 09:11:54 +0200

    Initial commit

---
+++ classes.js
@@ -0,0 +1,198 @@
+class EventBase {
+    target;
+    parentIdentifiers = [];
+    identifier;
+    intend = 3;
+    constructor(target)
+    {
+        if (!target) {
+            return;
+        }
+        this.target = target;
+        this.identifier = new IdentifierCache(target);
+    }
+    textContent()
+    {
+        return this.target ? this.target.textContent : '';
+    }
+    getTarget()
+    {
+        return this.target;
+    }
+    toString()
+    {
+        if (this.identifier.getId()) {
+            return this.identifier.toString();
+        }
+        const getParentIdentifiers = (element) => {
+            const parentIdentifier = new IdentifierCache(element.parentElement);
+            this.parentIdentifiers.push(parentIdentifier);
+            if (parentIdentifier.getId()) {
+                return;
+            }
+            if (this.parentIdentifiers.length > 2) {
+                return;
+            }
+            if (element.parentElement) {
+                getParentIdentifiers(element.parentElement);
+            }
+        };
+        getParentIdentifiers(this.target);
+        let resultString = '';
+        for (let i = this.parentIdentifiers.length - 1; i >= 0; i--) {
+            resultString += `${this.parentIdentifiers[i].toString()} `;
+        }
+        resultString += this.identifier.toString();
+        return resultString;
+    }
+}
+
+class WaitEvent extends EventBase {
+    constructor(target)
+    {
+        super(target);
+    }
+    toString()
+    {
+        return `$this->waitForPageLoad($page);`;
+    }
+}
+
+class InputElement extends EventBase {
+    result;
+    constructor(target)
+    {
+        super(target);
+        this.target.addEventListener('input', () => {this.result = this.target.value});
+    }
+    toString()
+    {
+        return `${' '.repeat(this.intend)}$this->findCssAndSetValue($page, ${super.toString()}, ${this.result});\n`;
+    }
+}
+
+class TextElement extends EventBase {
+    constructor(target)
+    {
+        super(target);
+    }
+    toString()
+    {
+        return `${' '.repeat(this.intend)}$this->findCssAndGetText($page, ${super.toString()});`;
+    }
+}
+
+class PressElement extends EventBase {
+    constructor(target)
+    {
+        super(target);
+    }
+    toString()
+    {
+        return `${' '.repeat(this.intend)}$this->findCssAndGetText($page, ${super.toString()});`;
+    }
+}
+
+class FunctionDeclaration extends EventBase {
+    name = '';
+    constructor(target, name)
+    {
+        super(target);
+        this.intend = 0;
+        this.name = name;
+    }
+    setName(newName)
+    {
+        this.name = newName;
+    }
+    toString()
+    {
+        return `
+        public function ${this.name}(): void
+        {`;
+    }
+}
+
+class WrapperElement extends EventBase {
+    events = [];
+    constructor(target)
+    {
+        super(target);
+    }
+    addEvent(event)
+    {
+        this.events.push(event);
+        this.displayPopup(event);
+    }
+    toString()
+    {
+        let result = '';
+        this.events.forEach(event => {
+            result += `${event.toString()}<br>`; 
+        });
+        return result;
+    }
+    displayPopup(eventBase)
+    {
+        const notification = document.createElement('dialog');
+        notification.className = 'js-prevent-check';
+        document.body.append(notification);
+        // Find the position and try to put the element there
+        const bounds = eventBase.getTarget()?.getBoundingClientRect();
+        if (bounds) {
+            notification.style.top = `${bounds.top}px`;
+            notification.style.left = `${bounds.left}px`;
+        } else {
+            notification.style.top = '50%';
+        }
+
+        notification.style.zIndex = 1000000;
+        notification.textContent = `Added event: ${eventBase.toString()}`;
+        notification.show();
+        setTimeout(() => {notification.close(); notification.remove();}, 2000);
+    }
+}
+
+class InitElement extends EventBase {
+    url;
+    constructor(target, url)
+    {
+        super(target);
+        this.url = url;
+    }
+    toString()
+    {
+        return `
+        ${' '.repeat(this.intend)}$session = $this->getMinkSession();<br>
+        ${' '.repeat(this.intend)}$session->visit($this->getVuFindUrl('${this.url}'));<br>
+        ${' '.repeat(this.intend)}$page = $session->getPage();`;
+    }
+}
+
+class IdentifierCache {
+    classList;
+    id;
+    tag;
+    constructor(element)
+    {
+        if (!element) {
+            return;
+        }
+        this.classList = element.classList;
+        this.id = element.getAttribute('id');
+        this.tag = element.tagName.toLowerCase();
+    }
+    getId()
+    {
+        return this.id;
+    }
+    toString()
+    {
+        const newList = [];
+        if (this.classList && this.classList.length) {
+            this.classList.forEach(l => newList.push(`.${l}`));
+        }
+        const finalId = this.id ? `#${this.id}` : '';
+        return `${this.tag}${finalId}${newList.join('')}`;
+    }
+}
---
+++ recorder.js
@@ -0,0 +1,158 @@
+const dialogTemplate = `
+    <h2>Mink test result</h2>
+    
+`;
+
+const optionTemplate = `
+    <h2>Options:</h2>
+    <ul>
+        <li><b>What is the name for the function for this test:</b></li>
+        <li><input class="js-function-name" type="text" placeholder="exampleFunctionName"></input></li>
+        <li><b>Write any overrides for configs in this text area:</b></li>
+        <li><textarea>Not yet implemented</textarea></li>
+        <li><button type="button" class="js-add-assert">Add an assert for test</button></li>
+        <li><button type="button" class="js-add-wait">Add a wait for page load event</button></li>
+    <ul>
+`;
+
+let isCapturing = false;
+
+let isMenuOpen = false;
+
+
+
+let optionDialog;
+
+function setInnerHtml(element, content)
+{
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = content;
+    element.replaceChildren(...tempDiv.childNodes);
+}
+
+
+
+const helpText = `<p>Addon for recording tests running! [F1] Open settings [F2] Show results [F3] Start/Stop recording events [F4] Open add event menu</p><div class="js-record-state"></div>`;
+
+// Add a text box up to the screen to give instructions
+const guidediv = document.createElement('div');
+guidediv.style.position = 'sticky';
+guidediv.style.width = '100%';
+guidediv.style.height = '50px';
+guidediv.style.display = 'flex';
+guidediv.style.justifyContent = 'center';
+guidediv.style.alignItems = 'center';
+guidediv.style.flexFlow = 'column';
+setInnerHtml(guidediv, helpText);
+recordStateDiv = guidediv.querySelector('.js-record-state');
+document.body.prepend(guidediv);
+let functionWrapper = new WrapperElement(document.body);
+let functionDeclaration = new FunctionDeclaration(undefined, '');
+
+functionWrapper.addEvent(functionDeclaration);
+// Set starting values for the function
+functionWrapper.addEvent(new InitElement(undefined, '/MyResearch/Profile'));
+
+document.addEventListener('click', (e) => {
+    if (!e.target) {
+        return;
+    }
+    getAnEventFromElement(e.target, true);
+}, true);
+function displayResults() {
+    isMenuOpen = true;
+    const resultDialog = document.createElement('dialog');
+    resultDialog.className = 'js-prevent-check';
+    document.body.appendChild(resultDialog);
+    setInnerHtml(resultDialog, functionWrapper.toString());
+    resultDialog.showModal();
+    resultDialog.addEventListener('close', () => {
+        isMenuOpen = false;
+        resultDialog.remove();
+    });
+}
+function getAnEventFromElement(element, allowParentCheck = false)
+{
+    if (!isCapturing)
+    {
+        return;
+    }
+    if (element.closest('.js-prevent-check')) {
+        return;
+    }
+    switch (element.tagName.toLowerCase()) {
+        case "input":
+            functionWrapper.addEvent(new InputElement(element));
+            return true;
+        case "a":
+        case "button":
+            functionWrapper.addEvent(new PressElement(element));
+            return true;
+        default:
+            if (allowParentCheck) {
+                let currentParent = element.parentElement;
+                let resultOfParent = getAnEventFromElement(currentParent);
+                if (!resultOfParent) {
+                    functionWrapper.addEvent(new TextElement(element));
+                }
+            }
+            return false;
+    }
+}
+
+function toggleRecording()
+{
+    isCapturing = !isCapturing;
+    recordStateDiv.textContent = `Is the addon recording: ${isCapturing}`;
+}
+
+// Add keypress handler for f[n] buttons
+document.addEventListener('keydown', (e) => {
+
+    switch (e.key) {
+        case "F1":
+            e.preventDefault();
+            openMenu();
+            // Open menu.
+            break;
+        case "F2":
+            e.preventDefault();
+            displayResults();
+            // Show results shortcut
+            break;
+        case "F3":
+            e.preventDefault();
+            toggleRecording();
+            break;
+        case "F4":
+            e.preventDefault();
+            showEventMenu();
+            break;
+        default:
+            break;
+    }
+});
+
+function openMenu()
+{
+    if (isMenuOpen) {
+        return;
+    }
+    isMenuOpen = true;
+    const optionDialog = document.createElement('dialog');
+    optionDialog.className = 'js-prevent-check';
+    document.body.appendChild(optionDialog);
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = optionTemplate;
+    optionDialog.replaceChildren(...tempDiv.childNodes);
+    optionDialog.querySelector('.js-function-name').addEventListener('input', (e) => {functionDeclaration.setName(e.target.value)});
+    optionDialog.querySelector('.js-add-wait').addEventListener('click', (e) => {
+        functionWrapper.addEvent(new WaitEvent());
+        optionDialog.close();
+    });
+    optionDialog.addEventListener('close', () => {
+        isMenuOpen = false;
+        optionDialog.remove();
+    });
+    optionDialog.showModal();
+}
